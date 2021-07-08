946d90108cfe778f9a5465308fa521cb833e8b07
mahfuz <mahfuz01bd@gmail.com>
2021-07-09 04:37:10 +0600

    included jquery

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 
+// Jquery
+import 'jquery'
+
 
 function App() {
   return (
     <div>
-
+      <p>loremadsfadsfasdfasdf</p>
     </div>
   );
 }
